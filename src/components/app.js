ea4936db73793e5eb122ec07b278f351384f3d98
Logan <Logan@Owners-MacBook-Pro.local>
2020-06-15 11:25:51 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Logan Beck Final Project</h1>
+      </div>
+    );
+  }
+}
